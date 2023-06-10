fdaadcbde8dee97890e2fd2bc250cbc603080bcc
Elisha <ukaigve1990@mail.ru>
2023-06-11 00:58:03 +0300

    фвв

--- WEB_lesson3/hi.js
+++ WEB_lesson3/hi.js
@@ -1,2 +1,13 @@
-let userName = prompt("Введите имя: ", "Имя");
-alert("Привет, " + userName + "!");
+// let userName = prompt("Введите имя: ", "Имя");
+// alert("Привет, " + userName + "!");
+
+
+let name = prompt("Введите ваше имя", "Имя");
+
+        function showName() {
+	let message = "Привет, "
+	alert(`${message + name}.`)
+
+    }
+
+    showName()
